1ca0d2bb31a2a8b87559b8c3e930c32495750d76
송영철 <whyseesong@naver.com>
2019-10-14 20:22:43 +0900

    1st expermintal training error fixed
    error was occured from mistyping...

--- kuchara.bomnal/code/FindMovieScheduleEveryConditionSatisfied.js
+++ kuchara.bomnal/code/FindMovieScheduleEveryConditionSatisfied.js
@@ -16,76 +16,76 @@ module.exports.function = function FindMovieScheduleEveryConditionSatisfied (mov
 
   let response = [
     {
-      movie: {
-        movieName: "조커",
-        duration: 130,
-        movieRating: "15세이용가",
-        director: '스티븐 스필버그',
-        actors: ['히스레저', '조셉 고든 래빗'],
-        genre: ['액션', '드라마'],
+      "movie": {
+        "movieName": "조커",
+        "duration": 130,
+        "movieRating": "15세관람가",
+        "director": '스티븐 스필버그',
+        "actors": ['히스레저', '조셉 고든 래빗'],
+        "genre": ['액션', '드라마'],
       },
-      startTime: dates.ZonedDateTime.now(),
-      endTime: dates.ZonedDateTime.now(),
-      totalSeat: 234,             // 총 좌석수
-      availableSeat: 123,             // 빈 좌석수
-      theater: {
-        theaterName: "영통 메가박스",
-        code: 1533,
-        regioncode: 25,
-        pos: {
-          xpos: 27.483674,
-          ypos: 57.123734,
+      // startTime: dates.ZonedDateTime.now(),
+      // endTime: dates.ZonedDateTime.now(),
+      "totalSeat": 234,             // 총 좌석수
+      "availableSeat": 123,             // 빈 좌석수
+      "theater": {
+        "theaterName": "영통 메가박스",
+        "theaterCode": 1533,
+        "regionCode": 25,
+        // "pos": {
+        //   "xpos": 27.483674,
+        //   "ypos": 57.123734,
+        // },
+        "brand": "megabox",
+        "theaterFlag": {
+          "locationalKeyword": ['영통', '수원', '경희대']
         },
-        brand: "megabox",
-        TheaterFlag: {
-          locationalKeyword: ['영통', '수원', '경희대']
-        },
       },
-      movieScheduleFlag: {
-        subtitle: true,               // 자막여부
-        dubbing: false,                // 더빙여부
-        digitalized: true,            // 디지털 / 필름
-        lateNight: false,              // 심야 여부
-        morning: false,   
+      "movieScheduleFlag": {
+        "subtitle": true,               // 자막여부
+        "dubbing": false,                // 더빙여부
+        "digitalized": true,            // 디지털 / 필름
+        "lateNight": false,              // 심야 여부
+        "morning": false,   
       },
-      room: "1관",                 // 상영관
-      code: 123123,  
+      "room": "1관",                 // 상영관
+      "movieCode": 123123,  
     },
     {
-      movie: {
-        movieName: "조커123",
-        duration: 140,
-        movieRating: "15세이용가",
-        director: '토드 스필버그',
-        actors: ['조셉 고든 래빗', '호아킨 피닉스'],
-        genre: ['액션', '드라마'],
+      "movie": {
+        "movieName": "조커123",
+        "duration": 140,
+        "movieRating": "15세관람가",
+        "director": '토드 스필버그',
+        "actors": ['조셉 고든 래빗', '호아킨 피닉스'],
+        "genre": ['액션', '드라마'],
       },
-      startTime: dates.ZonedDateTime.now(),
-      endTime: dates.ZonedDateTime.now(),
-      totalSeat: 234,             // 총 좌석수
-      availableSeat: 123,             // 빈 좌석수
-      theater: {
-        theaterName: "왕십리 CGV",
-        code: 1533,
-        regioncode: 25,
-        pos: {
-          xpos: 27.483674,
-          ypos: 57.123734,
+      // startTime: dates.ZonedDateTime.now(),
+      // endTime: dates.ZonedDateTime.now(),
+      "totalSeat": 234,             // 총 좌석수
+      "availableSeat": 123,             // 빈 좌석수
+      "theater": {
+        "theaterName": "왕십리 CGV",
+        "theaterCode": 1533,
+        "regionCode": 25,
+        // "pos": {
+        //   "xpos": 27.483674,
+        //   "ypos": 57.123734,
+        // },
+        "brand": "cgv",
+        "theaterFlag": {
+          "locationalKeyword": ['왕십리', '한양대', '성동구']
         },
-        brand: "CGV",
-        TheaterFlag: {
-          locationalKeyword: ['왕십리', '한양대', '성동구']
-        },
       },
-      movieScheduleFlag: {
-        subtitle: true,               // 자막여부
-        dubbing: false,                // 더빙여부
-        digitalized: true,            // 디지털 / 필름
-        lateNight: false,              // 심야 여부
-        morning: false,   
+      "movieScheduleFlag": {
+        "subtitle": true,               // 자막여부
+        "dubbing": false,                // 더빙여부
+        "digitalized": true,            // 디지털 / 필름
+        "lateNight": false,              // 심야 여부
+        "morning": false,   
       },
-      room: "7관",                 // 상영관
-      code: 234234,  
+      "room": "7관",                 // 상영관
+      "movieCode": 234234,  
     }
   ]
 
