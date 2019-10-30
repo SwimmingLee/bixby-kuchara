82e853526ae230a63555e8dd6a1d67599569c733
qhcjfdudn <jhjhyjin@naver.com>
2019-10-30 12:56:24 +0900

    makeTimeExpression error modified

--- kuchara.bomnal/code/Make/MakeDateTimeExpression.js
+++ kuchara.bomnal/code/Make/MakeDateTimeExpression.js
@@ -14,9 +14,9 @@ module.exports.function = function makeDateTimeExpression (dateTimeExpression, d
   console.log(detachedTime);
 
   if(typeof dateTimeExpression != 'undefined'){
-    dateTimeExpressionObj.dateTime.date.year = dateTimeExpression.date.year;
-    dateTimeExpressionObj.dateTime.date.month = dateTimeExpression.date.month;
-    dateTimeExpressionObj.dateTime.date.day = dateTimeExpression.date.day;
+    dateTimeExpressionObj.dateTime.date.year = dateTimeExpression.dateTime.date.year;
+    dateTimeExpressionObj.dateTime.date.month = dateTimeExpression.dateTime.date.month;
+    dateTimeExpressionObj.dateTime.date.day = dateTimeExpression.dateTime.date.day;
   } 
 
   // console.log(detachedTime.amPM);
@@ -41,7 +41,7 @@ module.exports.function = function makeDateTimeExpression (dateTimeExpression, d
 
     // 오후 태그가 달려있으면 시간에 12더하기
     if(detachedTime.amPM == 'Pm'){
-      hourin = hourin + 12;  
+      hourin = hourin + 12;
     } else {  // 현재시간이 인풋시간보다 뒷시간이면 시간에 12더하기
       let currenttime = dateTimeExpressionObj.dateTime.time.hour*60 + dateTimeExpressionObj.dateTime.time.minute*1;
       let inputtime = hourin*60 + minin;
