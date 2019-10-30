c8a4062c01d6b3b0a2059a8a9f1d23fc74326654
송영철 <whyseesong@naver.com>
2019-10-30 21:33:27 +0900

    training set 2 and isdomestic attribute added

--- kuchara.bomnal/code/Filter/FilterMovieWithNationAndThOSM.js
+++ kuchara.bomnal/code/Filter/FilterMovieWithNationAndThOSM.js
@@ -1,31 +1,51 @@
-module.exports.function = function filterMovieWithNationAndThOSM (theaterOrderedScheduleWithMovie, nation, exceptExpression) {
+module.exports.function = function filterMovieWithNationAndThOSM (theaterOrderedScheduleWithMovie, nation, isDomestic, exceptExpression) {
   let result = {
     movieOrderedSchedule: []
   };
-
-  // '제외하고', '빼고'와 같은 단어가 안들어온 경우
-  // 해당 장르만 보여준다.
-  if(typeof exceptExpression == 'undefined'){
-    theaterOrderedScheduleWithMovie.movieOrderedSchedule.forEach(function(mosElement){
-      if(mosElement.movie.nation.includes(nation)){
-        result.movieOrderedSchedule.push(mosElement);
-      }
-    })
-  } else {  
-    if(!exceptExpression){
+​
+​
+  // 우리나라 키워드가 없으면,
+  if(typeof isDomestic == 'undefined'){
+    // '제외하고', '빼고'와 같은 단어가 안들어온 경우
+    // 해당 장르만 보여준다.
+    if(typeof exceptExpression == 'undefined'){
       theaterOrderedScheduleWithMovie.movieOrderedSchedule.forEach(function(mosElement){
-      if(mosElement.movie.nation.includes(nation)){
-        result.movieOrderedSchedule.push(mosElement);
-      }
+        if(mosElement.movie.nation.includes(nation)){
+          result.movieOrderedSchedule.push(mosElement);
+        }
       })
-    } else {
-      theaterOrderedScheduleWithMovie.movieOrderedSchedule.forEach(function(mosElement){
-      if(!mosElement.movie.nation.includes(nation)){
-        result.movieOrderedSchedule.push(mosElement);
+    } else {  
+      if(!exceptExpression){
+        theaterOrderedScheduleWithMovie.movieOrderedSchedule.forEach(function(mosElement){
+        if(mosElement.movie.nation.includes(nation)){
+          result.movieOrderedSchedule.push(mosElement);
+        }
+        })
+      } else {
+        theaterOrderedScheduleWithMovie.movieOrderedSchedule.forEach(function(mosElement){
+        if(!mosElement.movie.nation.includes(nation)){
+          result.movieOrderedSchedule.push(mosElement);
+        }
+        })
       }
+    }
+​
+  } else {
+    // 우리나라 키워드가 들어오면,
+    if(isDomestic){
+      theaterOrderedScheduleWithMovie.movieOrderedSchedule.forEach(function(mosElement){
+        if(mosElement.movie.nation.includes("한국")){
+          result.movieOrderedSchedule.push(mosElement);
+        }
+      })
+    } else {    // 외국 키워드가 들어오면,
+      theaterOrderedScheduleWithMovie.movieOrderedSchedule.forEach(function(mosElement){
+        if(!mosElement.movie.nation.includes("한국")){
+          result.movieOrderedSchedule.push(mosElement);
+        }
       })
     }
   }
-
+​
   return result;
 }
--- kuchara.bomnal/code/Filter/FilterMovieWithNationAndTiOSM.js
+++ kuchara.bomnal/code/Filter/FilterMovieWithNationAndTiOSM.js
@@ -1,30 +1,51 @@
-module.exports.function = function filterMovieWithNationAndTiOSM (timeOrderedScheduleWithMovie, nation, exceptExpression) {
+module.exports.function = function filterMovieWithNationAndTiOSM (timeOrderedScheduleWithMovie, nation, isDomestic, exceptExpression) {
   let result = {
     movieOrderedSchedule: []
   };
-  // '제외하고', '빼고'와 같은 단어가 안들어온 경우
-  // 해당 장르만 보여준다.
-  if(typeof exceptExpression == 'undefined'){
-    timeOrderedScheduleWithMovie.movieOrderedSchedule.forEach(function(mosElement){
-      if(mosElement.movie.nation.includes(nation)){
-        result.movieOrderedSchedule.push(mosElement);
-      }
-    })
-  } else {  
-    if(!exceptExpression){
+​
+  // 우리나라 키워드가 없으면,
+  if(typeof isDomestic == 'undefined'){
+    // '제외하고', '빼고'와 같은 단어가 안들어온 경우
+    // 해당 장르만 보여준다.
+    if(typeof exceptExpression == 'undefined'){
       timeOrderedScheduleWithMovie.movieOrderedSchedule.forEach(function(mosElement){
-      if(mosElement.movie.nation.includes(nation)){
-        result.movieOrderedSchedule.push(mosElement);
-      }
+        if(mosElement.movie.nation.includes(nation)){
+          result.movieOrderedSchedule.push(mosElement);
+        }
       })
-    } else {
+    } else {  
+      if(!exceptExpression){
+        timeOrderedScheduleWithMovie.movieOrderedSchedule.forEach(function(mosElement){
+        if(mosElement.movie.nation.includes(nation)){
+          result.movieOrderedSchedule.push(mosElement);
+        }
+        })
+      } else {
+        timeOrderedScheduleWithMovie.movieOrderedSchedule.forEach(function(mosElement){
+        if(!mosElement.movie.nation.includes(nation)){
+          result.movieOrderedSchedule.push(mosElement);
+        }
+        })
+        
+      }
+    }
+​
+  } else {
+    // 우리나라 키워드가 들어오면,
+    if(isDomestic){
       timeOrderedScheduleWithMovie.movieOrderedSchedule.forEach(function(mosElement){
-      if(!mosElement.movie.nation.includes(nation)){
-        result.movieOrderedSchedule.push(mosElement);
-      }
+        if(mosElement.movie.nation.includes("한국")){
+          result.movieOrderedSchedule.push(mosElement);
+        }
       })
-      
+    } else {    // 외국 키워드가 들어오면,
+      timeOrderedScheduleWithMovie.movieOrderedSchedule.forEach(function(mosElement){
+        if(!mosElement.movie.nation.includes("한국")){
+          result.movieOrderedSchedule.push(mosElement);
+        }
+      })
     }
   }
+​
   return result;
 }
